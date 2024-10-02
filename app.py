4050188550e2211e7e34d7ad53598686454788fc
SLuckLeonard <samluckleonard@gmail.com>
2024-10-01 22:18:41 -0500

    Position Added
    Position Display

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request
-from utils.comparison import compare_players, get_player_id, get_player_headshot, get_team_logo
+from utils.comparison import compare_players, get_player_id, get_player_headshot, get_team_logo, get_player_pos
+from utils.api_calls import get_fantasy_point_projections
 
 app = Flask(__name__)
 
@@ -30,6 +31,12 @@ def compare():
     player_a_team_logo = get_team_logo(team1)
     player_b_team_logo = get_team_logo(team2)
 
+    player_a_projections = get_fantasy_point_projections(player_id=player_a_id)
+    player_b_projections = get_fantasy_point_projections(player_id=player_b_id)
+
+    player_a_pos = get_player_pos(player_a_projections)
+    player_b_pos = get_player_pos(player_b_projections)
+
     # Perform the comparison
     result = compare_players(player_a_id, player_b_id, week, player_a_name, player_b_name)
 
@@ -42,7 +49,9 @@ def compare():
         player_a_headshot=player_a_headshot,
         player_b_headshot=player_b_headshot,
         player_a_team_logo=player_a_team_logo,
-        player_b_team_logo=player_b_team_logo
+        player_b_team_logo=player_b_team_logo,
+        player_a_pos=player_a_pos,
+        player_b_pos=player_b_pos
     )
 
 
